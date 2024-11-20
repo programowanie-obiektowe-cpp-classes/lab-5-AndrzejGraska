51bb644cf50a437be07313f1dd78d73c172f4356
Andrzej Graska <graskaandrzej@gmail.com>
2024-11-20 23:23:32 +0100

    Zrobione

--- foo.hpp
+++ foo.hpp
@@ -5,8 +5,29 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+using namespace std;
+
+vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    vector<char> w1;
+    list<Human>::reverse_iterator it;
+
+    for(Human &e : people)
+    {
+        e.birthday();
+    }
+    
+    for (it = people.rbegin(); it != people.rend(); it++)
+    {
+        if(it->isMonster()==true)
+        {
+            w1.push_back('n');
+        }
+        else
+        {
+            w1.push_back('y');
+        }
+    }
+
+    return w1;
 }
